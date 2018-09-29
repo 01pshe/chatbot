1c66d80cb0b047bf8fbcebb1f65026d7660f15f8
MxWild <mxwild@gmail.com>
2018-09-29 10:22:13 +0300

    Start REST realize

---
+++ rest/src/main/java/com/vol/chatbot/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.vol.chatbot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+
+  @GetMapping
+  public String listAll() {
+    return "List All Users";
+  }
+
+}
